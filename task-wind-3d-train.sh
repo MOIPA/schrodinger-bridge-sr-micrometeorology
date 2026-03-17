70899e75a0bb2e419f9f3521cd77aaeebab61a93
tangrui <williantang@tencent.com>
2026-03-17 14:59:14 +0800

    add slurm run sh

---
+++ task-wind-3d-train.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -p amd_512
+#SBATCH -N 1
+#SBATCH -n 128
+#SBATCH -J tzq_3dwind_train
+source /public3/soft/modules/module.sh
+source /public3/soft/miniforge/24.11/etc/profile.d/conda.sh
+# 设置并行计算线程数量
+export OMP_NUM_THREADS=$SLURM_NPROCS
+echo "OMP_NUM_THREADS=$SLURM_NPROCS"
+conda activate tzq
+python scripts/train_schrodinger_bridge_model.py --config_path configs/config_wind_3d.yml --experiment_name ExperimentSchrodingerBridge3dWind --device cpu
