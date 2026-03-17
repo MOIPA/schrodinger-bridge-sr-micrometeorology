5a4175b479017b2f0eb087f28cbd261200992766
tangrui <williantang@tencent.com>
2026-03-17 15:07:02 +0800

    update config

--- task-wind-3d-train.sh
+++ task-wind-3d-train.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
-#SBATCH -p amd_512
-#SBATCH -N 1
-#SBATCH -n 128
-#SBATCH -J tzq_3dwind_train
-source /public3/soft/modules/module.sh
+#!/bin/bash                                                           
+#SBATCH -p amd_512          
+#SBATCH -N 1                           
+#SBATCH -n 128                          
+#SBATCH -J tzq_train_job                         
+source /public3/soft/modules/module.sh                                
 source /public3/soft/miniforge/24.11/etc/profile.d/conda.sh
-# 设置并行计算线程数量
 export OMP_NUM_THREADS=$SLURM_NPROCS
 echo "OMP_NUM_THREADS=$SLURM_NPROCS"
 conda activate tzq
